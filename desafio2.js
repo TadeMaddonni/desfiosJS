239d4ec91ed21ed7761fdeae8a86286c47412a6c
Tade Maddonni <tadeomaddonni@gmail.com>
2022-06-10 14:23:25 -0300

    arreglos

--- desafio2.js
+++ desafio2.js
@@ -2,17 +2,35 @@
 let numero = Number(prompt("Ingrese un numero"))
 
 if (isNaN(numero) || numero == ""){
-    alert("El dato ingresado no es un numero")
-    numero=Number(prompt("Por favor ingrese un numero"))
-} 
-
-while(numero != 0){
-    let numero2 = Number(prompt("ingrese otro numero"));
-    resultado =  numero + numero2;
-    console.log(`El resultado de la suma entre ${numero} y ${numero2} es ${resultado}`);
-    numero = Number(prompt("ingrese otro numero"));
-    if (isNaN(numero) || numero == ""){
+    if(numero==0){
+        alert("El numero ingresado es 0 por lo que no va a funcionar el programa")
+    }else{
         alert("El dato ingresado no es un numero")
         numero=Number(prompt("Por favor ingrese un numero"))
+    }
+} 
+
+
+while(numero != 0 || numero2 != 0){
+    let numero2 = Number(prompt("ingrese otro numero"));
+    if (isNaN(numero2) || numero2 == ""){
+        if(numero2==0){
+            alert("El numero ingresado es 0 por lo que no va a funcionar el programa");
+            break;
+        }else{
+            alert("El dato ingresado no es un numero")
+            numero2=Number(prompt("Por favor ingrese un numero"))
+        }
+    } 
+    resultado =  numero + numero2;
+    console.log(`El resultado de la suma entre ${numero} y ${numero2} es ${resultado}`);
+    numero = Number(prompt("ingrese un nuevo numero"));
+    if (isNaN(numero) || numero == ""){
+        if(numero==0){
+            alert("El numero ingresado es 0 por lo que no va a funcionar el programa")
+        }else{
+            alert("El dato ingresado no es un numero")
+            numero=Number(prompt("Por favor ingrese un numero"))
+        }
     } 
 }
