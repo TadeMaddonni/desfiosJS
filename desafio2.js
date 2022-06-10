509ff649f30a49cee348f3ecd434b1606606e945
Tade Maddonni <tadeomaddonni@gmail.com>
2022-06-10 14:26:56 -0300

    msj x consola

--- desafio2.js
+++ desafio2.js
@@ -27,7 +27,9 @@ while(numero != 0 || numero2 != 0){
     numero = Number(prompt("ingrese un nuevo numero"));
     if (isNaN(numero) || numero == ""){
         if(numero==0){
-            alert("El numero ingresado es 0 por lo que no va a funcionar el programa")
+            alert("El numero ingresado es 0 por lo que no va a funcionar el programa");
+            console.log("El usuario finalizó el algoritmo")
+            break;
         }else{
             alert("El dato ingresado no es un numero")
             numero=Number(prompt("Por favor ingrese un numero"))
